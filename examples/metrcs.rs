675f53cc4a234c3398a4f001277b90fdba4130a8
wangxiaochuang <932698529@qq.com>
2024-05-01 06:54:16 +0800

    feature: add mutex lock support

---
+++ examples/metrcs.rs
@@ -0,0 +1,53 @@
+use std::{thread, time::Duration};
+
+use anyhow::Result;
+use concurrency::Metrics;
+use rand::Rng;
+
+const N: usize = 2;
+const M: usize = 4;
+fn main() -> Result<()> {
+    let metrics = Metrics::new();
+
+    println!("{:?}", metrics.snapshot());
+
+    for idx in 0..N {
+        task_worker(idx, metrics.clone())?;
+    }
+
+    for _ in 0..M {
+        request_worker(metrics.clone())?;
+    }
+
+    loop {
+        thread::sleep(Duration::from_secs(5));
+        println!("{:?}", metrics.snapshot());
+    }
+}
+
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+    Ok(())
+}
+
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
+    });
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod matrix;
+mod metrics;
 mod vector;
 
 pub use matrix::multiply;
 pub use matrix::Matrix;
+pub use metrics::Metrics;
 pub use vector::Vector;
